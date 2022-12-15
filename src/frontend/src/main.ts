21cc827b8f745041ef590fc671dc65ff14e4b058
stvoid <stvoid.it@yandex.ru>
2022-12-15 13:20:27 +0300

    add SASS

--- src/frontend/src/main.ts
+++ src/frontend/src/main.ts
@@ -1,4 +1,4 @@
 import { createApp } from "vue";
 import App from "@/App.vue";
-import "bootstrap/dist/css/bootstrap.min.css";
+import "@/scss/styles.scss";
 createApp(App).mount("#app");
--- src/frontend/vite.config.ts
+++ src/frontend/vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import path from "path"
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,28 +8,32 @@ export default defineConfig({
   server: {
     https: false,
     proxy: {
-        "/api": {
-            target: "http://0.0.0.0:8080"
-        }
+      "/api": {
+        target: "http://0.0.0.0:8080"
+      }
     },
     host: "0.0.0.0",
     port: 3000
-    },
-    build: {
-        target: "modules",
-        outDir: "dist",
-        manifest: false,
-        minify: "esbuild",
-        emptyOutDir: true,
-        sourcemap: false,
-        cssCodeSplit: false,
-    },
-    resolve: {
-        alias: [
-            {
-                find: "@",
-                replacement: path.resolve(__dirname, "src")
-            }
-        ]
-    }
-})
+  },
+  build: {
+    target: "modules",
+    outDir: "dist",
+    manifest: false,
+    minify: "esbuild",
+    emptyOutDir: true,
+    sourcemap: false,
+    cssCodeSplit: false
+  },
+  resolve: {
+    alias: [
+      {
+        find: "@",
+        replacement: path.resolve(__dirname, "src")
+      },
+      {
+        find: "~bootstrap",
+        replacement: path.resolve(__dirname, "node_modules/bootstrap")
+      }
+    ]
+  }
+});
